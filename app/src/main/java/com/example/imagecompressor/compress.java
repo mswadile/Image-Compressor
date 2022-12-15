3ebe1609601bb4a18cbb69b43c8e8f8fceeacc47
mswadile <mwadile03@gmail.com>
2022-12-15 10:12:48 +0530

    added 2 activities

---
+++ app/src/main/java/com/example/imagecompressor/compress.java
@@ -0,0 +1,14 @@
+package com.example.imagecompressor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class compress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compress);
+    }
+}
